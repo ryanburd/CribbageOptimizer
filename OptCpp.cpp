bf2d4b9ecc16df6bb091443ea8107335029f25e3
ryanburd <ryankburdick@gmail.com>
2023-02-16 23:10:23 -0500

    Fixing deck creation

--- OptCpp.cpp
+++ OptCpp.cpp
@@ -4,25 +4,48 @@
 #include <cstdlib>
 #include <ctime>
 
-using namespace std;
+// using namespace std;
 
 int main() {
     /* Build the deck of cards.
     *  Face cards will be assigned a number for ease of calculating points from
     *  runs: Jack = 11, Queen = 12, King = 13
     */
-    string suits[4] = {"S", "C", "H", "D"};
-    int values[13];
-    iota(values, values + 13, 1);
-    for (auto i : values)
-        cout << i << endl;
+    struct card {
+        enum class CardSuit : unsigned char {
+            spades,
+            clubs,
+            hearts,
+            diamonds
+        };
+        enum class CardValue : unsigned char {
+            ace = 1,
+            two,
+            three,
+            four,
+            five,
+            six,
+            seven,
+            eight,
+            nine,
+            ten,
+            jack,
+            queen,
+            king
+        };
+        CardSuit suit;
+        CardValue value;
+    };
+    card AceSpade;
+    AceSpade.suit = card::CardSuit::spades;
+    AceSpade.value = card::CardValue::ace;
 
     /* Randomly choose the dealer between two players:
     *  0 for self, 1 for opponent
     */
     srand(time(nullptr));
     short dealer = rand() % 2;
-    cout << dealer;
+    std::cout << dealer;
 
     return 0;
 }
